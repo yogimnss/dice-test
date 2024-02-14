da01a309eef5e275a1563f5d9a04a633079dfccb
yogimnss <yogeshgiri9151@gmail.com>
2024-02-14 15:43:40 +0530

    Added docker file for image generation as well as postman collection

--- src/main/java/com/dice/test/producer/WeatherForCastProducer.java
+++ src/main/java/com/dice/test/producer/WeatherForCastProducer.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
